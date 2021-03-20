9acf80ed6e4e49fe949510c4126d70c2c29af15e
Ingrid Rebecca Abraham <intarga@protonmail.com>
2021-03-20 15:15:37 +0100

    server: break implementation out into library

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,27 +1,6 @@
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use chatbot::server;
 
 fn main() {
-    let listener = TcpListener::bind("localhost:7878").unwrap();
-
-    for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
-    }
+    server::serve();
 }
 
-fn handle_connection(stream: TcpStream) {
-    let mut reader = BufReader::new(stream);
-    let mut header = String::with_capacity(1024);
-
-    reader.read_line(&mut header).unwrap();
-    header.pop();
-
-    println!("Header: {}", header);
-
-    match header.as_str() {
-        "WRITE" => println!("Write!"),
-        _ => println!("Header not recognised...")
-    }
-}
