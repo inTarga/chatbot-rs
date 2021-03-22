73e5b7d4dfab7bf9a90f8b1d45170e6112bca5de
Ingrid Rebecca Abraham <intarga@protonmail.com>
2021-03-22 01:41:10 +0100

    client: enable user to push messages to the log

--- src/client.rs
+++ src/client.rs
@@ -27,19 +27,28 @@ pub fn run() {
     let mut stdin = termion::async_stdin().keys();
 
     let mut msg_buf = Buf::init();
+    let mut msg_log: Vec<String> = Vec::new();
 
     loop {
-        redraw(&mut stdout, height, width, &msg_buf);
-        let quit = handle_input(&mut stdin, &mut msg_buf);
+        //Draw the screen
+        redraw(&mut stdout, height, width, &msg_buf, &msg_log);
 
-        if quit {
-            break;
+        //Handle input
+        if let Some(action) = handle_input(&mut stdin, &mut msg_buf) {
+            match action {
+                Action::Quit => break,
+                Action::Clear => {
+                    msg_log.push(String::from(format!("{}", msg_buf)));
+                    msg_buf.clear();
+                }
+            }
         }
+
         thread::sleep(Duration::from_millis(10));
     }
 }
 
-fn redraw(stdout: &mut RawTerminal<Stdout>, height: u16, width: u16, msg_buf: &Buf) {
+fn redraw(stdout: &mut RawTerminal<Stdout>, height: u16, width: u16, msg_buf: &Buf, msg_log: &Vec<String>) {
     //Draw divider
     write!(stdout, "{}{}{}",
              cursor::Goto(0, height-2),
@@ -60,27 +69,36 @@ fn redraw(stdout: &mut RawTerminal<Stdout>, height: u16, width: u16, msg_buf: &B
 
     //Draw log
     for i in 0..height-3 {
-        write!(stdout, "{}{}message{}",
+        if usize::from(i) >= msg_log.len() {
+            break
+        }
+
+        write!(stdout, "{}{}{}",
                  cursor::Goto(0, height-(i+3)),
                  clear::CurrentLine,
-                 i.to_string(),
+                 msg_log[msg_log.len() - (usize::from(i) + 1)],
         ).unwrap();
     }
 
     stdout.flush().unwrap();
 }
 
-fn handle_input(keys: &mut Keys<termion::AsyncReader>, msg_buf: &mut Buf) -> bool {
-    let input = keys.next();
-    if let Some(Ok(key)) = input {
+fn handle_input(keys: &mut Keys<termion::AsyncReader>, msg_buf: &mut Buf) -> Option<Action> {
+    //TODO: handle more keys at once?
+    if let Some(Ok(key)) = keys.next() {
         match key {
-            //Key::Char('q') => process::exit(0),
-            Key::Ctrl('c') => return true, //quit
+            Key::Ctrl('c') => return Some(Action::Quit),
+            Key::Char('\n') => return Some(Action::Clear),
             Key::Char(c) => msg_buf.insert(c),
             _ => (),
         }
     }
-    return false;
+    return None;
+}
+
+enum Action {
+    Quit,
+    Clear,
 }
 
 struct Buf {
@@ -93,9 +111,19 @@ impl Buf {
         Buf{buffer: [' '; 1024], head: 0}
     }
 
+    //TODO: handle overflow
     fn insert(&mut self, c: char) {
         self.buffer[self.head] = c;
         self.head += 1;
+    }
+
+    //TODO: fn back
+
+    fn clear(&mut self) {
+        for i in 0..(self.head) {
+            self.buffer[i] = ' ';
+        }
+        self.head = 0;
     }
 }
 
